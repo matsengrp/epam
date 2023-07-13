7f7a8a9d44a1679af71d6afa2521eb5d89d5dcbf
ksung25 <kevin.kh.sung@gmail.com>
2023-07-13 11:06:12 -0700

    proper encode/decode test, test sequence lengths are multiples of 3

--- tests/test_encode_decode.py
+++ tests/test_encode_decode.py
@@ -6,31 +6,12 @@ from epam.models import AbLang
 infilename = "data/parent-child-example.csv"
 outfilename = "tests/matrices.hdf5"
 
-ablang_heavy = AbLang(chain="heavy")
-ablang_heavy.write_probability_matrices(infilename, outfilename)
+def test_encode_decode():
+    # encode
+    ablang_heavy = AbLang(chain="heavy")
+    ablang_heavy.write_probability_matrices(infilename, outfilename)
 
-test_site = 23
-test_aa = "P"
-prob_aa_index = ablang_heavy.aa_str_sorted.index(test_aa)
-
-print(ablang_heavy.aa_str_sorted, prob_aa_index)
-with h5py.File(outfilename, "r") as testfile:
-    pcp_filename = testfile.attrs["pcp_filename"]
-    if testfile.attrs["checksum"] != generate_file_checksum(pcp_filename):
-        raise ValueError("checksum fail!")
-
-    pcp_filename = testfile.attrs["pcp_filename"]
-    pcp_df = pd.read_csv(pcp_filename, index_col=0)
-
-    for key in testfile.keys():
-        grp = testfile[key]
-        index = grp.attrs["pcp_index"]
-        row = pcp_df.loc[index]
-        print(
-            index,
-            row["sample_id"],
-            row["family"],
-            row["v_gene"],
-            f"prob({test_aa},{test_site}) =",
-            grp["data"][prob_aa_index, test_site],
-        )
+    # decode
+    with h5py.File(outfilename, "r") as testfile:
+        pcp_filename = testfile.attrs["pcp_filename"]
+        assert testfile.attrs["checksum"] == generate_file_checksum(pcp_filename)
