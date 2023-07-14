eaebb254af9b3b8fbcfc0403aa6146567b0786df
Mackenzie Johnson <mmjohn@utexas.edu>
2023-07-14 11:58:03 -0700

    Updated plan after chatting with Kevin

--- epam/evaluation.py
+++ epam/evaluation.py
@@ -20,15 +20,28 @@ def evaluate(prob_mat_filename):
         if prob_mat_file.attrs["checksum"] != generate_file_checksum(pcp_filename):
             raise ValueError(f"checksum failed for {pcp_filename}.")
 
+        # call function
+
+        # output metrics in csv
+
+
+# calculated for full data set - loop through data set for each metric
+def sub_accuracy
+
 
 
 # read in prob_match for one PCP
-# read in sequence data for child sequence and number of mutations
-# translate child sequence from nuc to aa
-# given n substitutions observed in the child, find top n probable substitutions
-# compare predicted sequence with child sequence:
+# read in sequence data for child sequence and parent sequence
+# translate child sequence from nuc to aa,  
+# location/number of mutations in child (as scanning, track substitions)
+# evaluate child sequence:
     # for sub accuracy:
     # find sites that mutated in predicted sequence
     # find number of mutated sites that match child aa
     # take ratio of number of matches/number of mutated sites
 
+
+#for matrix in prob_mat_file.keys() # gives group name / 1 matrix
+
+# given n substitutions observed in the child, find top n probable substitutions/create predicted seq
+# compare predicted sequence with child sequence:
