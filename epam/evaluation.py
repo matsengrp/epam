cff47dec3aba5b33441baae601de06ce71a9bdb8
Mackenzie Johnson <mmjohn@utexas.edu>
2023-07-26 19:23:50 -0700

    Completed remaining PR comments; more functionalization and unit testings, less comments with more clear naming conventions

--- epam/evaluation.py
+++ epam/evaluation.py
@@ -8,51 +8,65 @@ from epam.models import *
 from epam.sequences import translate_sequences
 
 
-def evaluate(prob_mat_filename, outfilename):
+def evaluate(prob_mat_path, model_performance_path):
     """
     Evaluate model predictions against reality in parent-child pairs (PCPs)
-    Function should be model-agnositic and currently limited to substitution accuracy
+    Function is model-agnositic and currently limited to substitution accuracy
     Output to CSV with columns for the different metrics and a row per data set
 
     Parameters:
-    prob_mat_filename (str): file name of probability matrix for parent-child pairs
-    outfilename (str): file name for output of model performance metrics
+    prob_mat_path (str): path to probability matrix for parent-child pairs
+    outfilename (str): path to output for model performance metrics
     """
-    with h5py.File(prob_mat_filename, "r") as prob_mat_file:
-        pcp_filename = prob_mat_file.attrs["pcp_filename"]
+    pcp_path = pcp_path_of_prob_mat_path(prob_mat_path)
 
-        # make sure probablity matrix matches PCP
-        if prob_mat_file.attrs["checksum"] != generate_file_checksum(pcp_filename):
-            raise ValueError(f"checksum failed for {pcp_filename}.")
+    sub_acc = calculate_sub_accuracy(prob_mat_path)
 
-        # call metric functions
-        sub_acc = calculate_sub_accuracy(prob_mat_filename)
+    model_performance = pd.DataFrame(
+        {
+            "data_set": [pcp_path],
+            "model": ["ablang"],  # Issue 8: hard coded for the moment
+            "sub_accuracy": [sub_acc],
+        }
+    )
 
-        # output metrics to csv
-        model_performance = pd.DataFrame(
-            {
-                "data_set": [pcp_filename], 
-                "model": ["ablang"],  # Issue 8: hard coded for the moment
-                "sub_accuracy": [sub_acc],
-            }
-        )
-        model_performance.to_csv(outfilename, index=False)
+    model_performance.to_csv(model_performance_path, index=False)
 
 
-def calculate_sub_accuracy(prob_mat_file):
+def pcp_path_of_prob_mat_path(prob_mat_path):
+    """
+    Return the path to the PCP file that matches the probability matrix
+    Check SHAs to ensure the PCP file matches the probability matrix
+
+    Parameters:
+    prob_mat_path (str): path to probability matrix for parent-child pairs
+
+    Returns:
+    pcp_path (str): path to parent-child pairs
+    """
+    with h5py.File(prob_mat_path, "r") as prob_mat_file:
+        pcp_path = prob_mat_file.attrs["pcp_filename"]
+
+        if prob_mat_file.attrs["checksum"] != generate_file_checksum(pcp_path):
+            raise ValueError(f"checksum failed for {pcp_path}.")
+
+    return pcp_path
+
+
+def calculate_sub_accuracy(prob_mat_path):
     """
     Calculate substitution accuracy for all PCPs in one data set/HDF5 file
     Returns substitution accuracy score for use in evaluate() and reporting all files
 
     Parameters:
-    prob_mat_filename (str): file name of probability matrices for parent-child pairs
+    prob_mat_path (str): path to probability matrices for parent-child pairs
 
     Returns:
     sub_accuracy (float): calculated substitution accuracy for data set of PCPs
     """
-    with h5py.File(prob_mat_file, "r") as matfile:
-        pcp_filename = matfile.attrs["pcp_filename"]
-        pcp_df = pd.read_csv(pcp_filename, index_col=0)
+    with h5py.File(prob_mat_path, "r") as matfile:
+        pcp_path = pcp_path_of_prob_mat_path(prob_mat_path)
+        pcp_df = pd.read_csv(pcp_path, index_col=0)
 
         num_sub_total = 0
         num_sub_correct = 0
@@ -65,31 +79,45 @@ def calculate_sub_accuracy(prob_mat_file):
             parent_nt, child_nt = pcp_df.loc[index, ["orig_seq", "mut_seq"]]
             [parent_aa, child_aa] = translate_sequences([parent_nt, child_nt])
 
-            assert len(parent_aa) == len(child_aa)
+            assert len(parent_aa) == len(
+                child_aa
+            ), "The parent and child amino acid sequences are not the same length."
 
             for i in range(len(parent_aa)):
                 if parent_aa[i] != child_aa[i]:
                     num_sub_total += 1
-                    # get the column of the probability matrix corresponding to amino acid site of interest
-                    # sort the probabilities (lowest to highest) and return the result as ordered list of indices
-                    # reverse the order of the list of indices (argsort() only sorts by lowest to highest)
-                    prob_sorted_aa_indices = matrix[:, i].argsort()[
-                        ::-1
-                    ]  # code from Kevin
-                    # string ranked aa string for easy eval
-                    pred_aa_ranked = "".join(
-                        (np.array(list(aa_str_sorted))[prob_sorted_aa_indices])
+
+                    pred_aa_sub = highest_ranked_substitution(
+                        matrix[:, i], parent_aa, i
                     )
-                    # get AA sequence predicted by model
-                    # force a substitution if model predicts same aa as parent
-                    # if highest prob aa doesn't match parent, use it
-                    if pred_aa_ranked[0] == parent_aa[i]:
-                        pred_aa_sub = pred_aa_ranked[1]
-                    elif pred_aa_ranked[0] != parent_aa[i]:
-                        pred_aa_sub = pred_aa_ranked[0]
-                    # if aa matches predicted aa (given a substitution), add to num_sub_correct
+
                     if pred_aa_sub == child_aa[i]:
                         num_sub_correct += 1
 
         sub_accuracy = num_sub_correct / num_sub_total
+
         return sub_accuracy
+
+
+def highest_ranked_substitution(matrix_i, parent_aa, i):
+    """
+    Return the highest ranked substitution for a given parent-child pair
+
+    Parameters:
+    matrix_i (np.array): probability matrix for parent-child pair at aa site i
+    parent_aa (str): parent amino acid sequence
+    i (int): index of amino acid site substituted
+
+    Returns:
+    pred_aa_sub (str): predicted amino acid substitution (most likely non-parent aa)
+    """
+    prob_sorted_aa_indices = matrix_i.argsort()[::-1]
+
+    pred_aa_ranked = "".join((np.array(list(aa_str_sorted))[prob_sorted_aa_indices]))
+
+    if pred_aa_ranked[0] == parent_aa[i]:
+        pred_aa_sub = pred_aa_ranked[1]
+    elif pred_aa_ranked[0] != parent_aa[i]:
+        pred_aa_sub = pred_aa_ranked[0]
+
+    return pred_aa_sub
--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -4,28 +4,68 @@ from epam.utils import generate_file_checksum
 from epam.models import AbLang
 from epam.evaluation import *
 
-infilename = "data/parent-child-example.csv"
-outfilename = "tests/matrices.hdf5"
-evalfilename = "tests/model_performance.csv"
+example_pcp_path = "data/eval-parent-child-example.csv"
+example_prob_mat_path = "tests/eval-matrices.hdf5"
+example_model_eval_path = "tests/model-performance.csv"
+correct_model_eval_path = "tests/eval-model-performance.csv"
 
 
-# this test is probably not necessary
 def test_calculate_substitution_accuracy():
     # encode
     ablang_heavy = AbLang(chain="heavy")
-    ablang_heavy.write_probability_matrices(infilename, outfilename)
+    ablang_heavy.write_probability_matrices(example_pcp_path, example_prob_mat_path)
 
     # evaluate metrics:
     # substitution accuracy
-    assert calculate_sub_accuracy(outfilename) == 41 / 99
+    assert calculate_sub_accuracy(example_prob_mat_path) == 4 / 13
 
 
-# simple test of main function, should replace with something smarter and check file contents
 def test_evaluate():
     # encode example
     ablang_heavy = AbLang(chain="heavy")
-    ablang_heavy.write_probability_matrices(infilename, outfilename)
+    ablang_heavy.write_probability_matrices(example_pcp_path, example_prob_mat_path)
 
-    # ensure evaluation script produces csv:
-    evaluate(outfilename, evalfilename)
-    assert os.path.exists(evalfilename) == True
+    # ensure evaluation script produces csv
+    evaluate(example_prob_mat_path, example_model_eval_path)
+    assert os.path.exists(example_model_eval_path) == True
+
+    # ensure output produced matches expect output
+    assert generate_file_checksum(example_model_eval_path) == generate_file_checksum(
+        correct_model_eval_path
+    )
+
+
+def test_highest_ranked_substitution():
+    # create example matrix for two sites
+    example_matrix = np.array(
+        [
+            [0.6, 0.1],  # A
+            [0, 0],
+            [0.3, 0],  # D
+            [0, 0],
+            [0, 0],
+            [0.1, 0.5],  # G
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0.4],  # Q
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+        ]
+    )
+
+    parent_aa = "AQ"
+
+    # case when highest predicted aa is parent aa, select next highest
+    assert highest_ranked_substitution(example_matrix[:, 0], parent_aa, 0) == "D"
+
+    # case when highest predicted aa is not parent aa, select highest
+    assert highest_ranked_substitution(example_matrix[:, 1], parent_aa, 1) == "G"
