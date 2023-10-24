b0f2def14d14986e49251cbfca9a7d736fadcff7
Erick Matsen <ematsen@gmail.com>
2023-10-24 08:28:05 -0700

    molevol tests

--- tests/test_molevol.py
+++ tests/test_molevol.py
@@ -1,9 +1,9 @@
-import numpy as np
+import torch
 import epam.molevol as molevol
 from epam.models import SHMple
 
 from epam.sequences import (
-    nt_idx_array_of_str,
+    nt_idx_tensor_of_str,
     translate_sequences,
     AA_STR_SORTED,
     CODONS,
@@ -12,17 +12,17 @@ from epam.sequences import (
 
 # These happen to be the same as some examples in test_models.py but that's fine.
 # If it was important that they were shared, we should put them in a conftest.py.
-ex_mut_probs = np.array([0.01, 0.02, 0.03])
-ex_sub_probs = np.array(
+ex_mut_probs = torch.tensor([0.01, 0.02, 0.03])
+ex_sub_probs = torch.tensor(
     [[0.0, 0.3, 0.5, 0.2], [0.4, 0.0, 0.1, 0.5], [0.2, 0.3, 0.0, 0.5]]
 )
-ex_parent_codon_idxs = nt_idx_array_of_str("ACG")
+ex_parent_codon_idxs = nt_idx_tensor_of_str("ACG")
 parent_nt_seq = "CAGGTGCAGCTGGTGGAG"  # QVQLVE
 weights_path = "data/shmple_weights/my_shmoof"
 
 
 def test_build_mutation_matrix():
-    correct_tensor = np.array(
+    correct_tensor = torch.tensor(
         [
             # probability of mutation to each nucleotide (first entry in the first row
             # is probability of no mutation)
@@ -31,27 +31,28 @@ def test_build_mutation_matrix():
             [0.006, 0.009, 0.97, 0.015],
         ]
     )
-    assert np.allclose(
-        correct_tensor,
-        molevol.build_mutation_matrices(
-            ex_parent_codon_idxs[np.newaxis, ...],
-            ex_mut_probs[np.newaxis, ...],
-            ex_sub_probs[np.newaxis, ...],
-        ).squeeze(),
-    )
+
+    computed_tensor = molevol.build_mutation_matrices(
+        ex_parent_codon_idxs.unsqueeze(0),
+        ex_mut_probs.unsqueeze(0),
+        ex_sub_probs.unsqueeze(0),
+    ).squeeze()
+
+    assert torch.allclose(correct_tensor, computed_tensor)
 
 
 def test_normalize_sub_probs():
-    parent_idxs = nt_idx_array_of_str("AC")
-    sub_probs = np.array([[0.2, 0.3, 0.4, 0.1], [0.1, 0.2, 0.3, 0.4]])
+    parent_idxs = nt_idx_tensor_of_str("AC")
+    sub_probs = torch.tensor([[0.2, 0.3, 0.4, 0.1], [0.1, 0.2, 0.3, 0.4]])
 
-    expected_normalized = np.array([[0.0, 0.375, 0.5, 0.125], [0.125, 0.0, 0.375, 0.5]])
-
+    expected_normalized = torch.tensor(
+        [[0.0, 0.375, 0.5, 0.125], [0.125, 0.0, 0.375, 0.5]]
+    )
     normalized_sub_probs = molevol.normalize_sub_probs(parent_idxs, sub_probs)
 
     assert normalized_sub_probs.shape == (2, 4), "Result has incorrect shape"
-    np.testing.assert_allclose(
-        normalized_sub_probs, expected_normalized, rtol=1e-6
+    assert torch.allclose(
+        normalized_sub_probs, expected_normalized
     ), "Unexpected normalized values"
 
 
@@ -61,7 +62,7 @@ def iterative_aaprob_of_mut_and_sub(parent_codon, mut_probs, sub_probs):
     """
     aa_probs = {}
     for aa in AA_STR_SORTED:
-        aa_probs[aa] = 0
+        aa_probs[aa] = 0.0
 
     # iterate through all possible child codons
     for child_codon in CODONS:
@@ -83,9 +84,9 @@ def iterative_aaprob_of_mut_and_sub(parent_codon, mut_probs, sub_probs):
 
     # need renormalization factor so that amino acid probabilities sum to 1,
     # since probabilities to STOP codon are dropped
-    psum = np.sum([aa_probs[aa] for aa in aa_probs.keys()])
+    psum = sum(aa_probs.values())
 
-    return np.array([aa_probs[aa] / psum for aa in AA_STR_SORTED])
+    return torch.tensor([aa_probs[aa] / psum for aa in AA_STR_SORTED])
 
 
 def test_aaprob_of_mut_and_sub():
@@ -93,17 +94,17 @@ def test_aaprob_of_mut_and_sub():
     [rates], [subs] = shmple_shmoof.model.predict_mutabilities_and_substitutions(
         [parent_nt_seq], [0.1]
     )
-    mut_probs = 1.0 - np.exp(-rates.squeeze())
+    mut_probs = 1.0 - torch.exp(-torch.tensor(rates.squeeze()))
     parent_codon = parent_nt_seq[0:3]
-    parent_codon_idxs = nt_idx_array_of_str(parent_codon)
+    parent_codon_idxs = nt_idx_tensor_of_str(parent_codon)
     codon_mut_probs = mut_probs[0:3]
-    codon_subs = subs[0:3]
+    codon_subs = torch.tensor(subs[0:3])
 
-    assert np.allclose(
+    assert torch.allclose(
         iterative_aaprob_of_mut_and_sub(parent_codon, codon_mut_probs, codon_subs),
         molevol.aaprob_of_mut_and_sub_v(
-            parent_codon_idxs[np.newaxis, ...],
-            codon_mut_probs[np.newaxis, ...],
-            codon_subs[np.newaxis, ...],
+            parent_codon_idxs.unsqueeze(0),
+            codon_mut_probs.unsqueeze(0),
+            codon_subs.unsqueeze(0),
         ).squeeze(),
     )
