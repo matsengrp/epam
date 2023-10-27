2871b21a73e97b11cf87187f35efcdd30d806c4c
Erick Matsen <ematsen@gmail.com>
2023-10-27 09:53:52 -0700

    reasonable first go at dnsm.py

--- epam/dnsm.py
+++ epam/dnsm.py
@@ -8,29 +8,39 @@ We'll use these conventions:
 
 """
 
+import numpy as np
+
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch import Tensor
 
 from epam import molevol
 from epam.models import TorchModel, MutSel
 
+import epam.sequences as sequences
 
 
 class TransformerBinarySelectionModel(nn.Module, TorchModel):
     """A transformer-based model for binary selection.
-    
+
     This is a model that takes in a batch of one-hot encoded sequences and outputs a binary selection matrix.
 
     See forward() for details.
     """
-    def __init__(self, model_dim, head_count, layer_count):
+
+    def __init__(self, nhead, dim_feedforward, layer_count):
         super().__init__()
         # batch_first means that we have data laid out in terms of (batch, sequence_length, features)
-        self.encoder_layer = nn.TransformerEncoderLayer(model_dim, head_count, batch_first=True)
+        self.encoder_layer = nn.TransformerEncoderLayer(
+            20, nhead, dim_feedforward, batch_first=True
+        )
         # This just makes a stack of layer_count of the encoder_layer.
         self.encoder = nn.TransformerEncoder(self.encoder_layer, layer_count)
-        self.linear = nn.Linear(model_dim, 1)
+        self.linear = nn.Linear(dim_feedforward, 1)
+        self.to(self.device)
 
-    def forward(self, src):
+    def forward(self, parent_onehots: Tensor):
         """Build a binary selection matrix from a one-hot encoded parent sequence.
 
         Parameters:
@@ -39,38 +49,55 @@ class TransformerBinarySelectionModel(nn.Module, TorchModel):
         Returns:
             A tensor of shape (B, L, 1) representing the level of selection for each amino acid site.
         """
-        out = self.encoder(src)
+        out = self.encoder(parent_onehots)
         out = self.linear(out)
         return torch.sigmoid(out).squeeze(-1)
 
-    def nograd_forward_of_aa_string(aa_str):
-        """Do the forward method without gradients from an amino acid string.
+    def p_substitution_of_aa_str(self, aa_str: str):
+        """Do the forward method without gradients from an amino acid string and convert to numpy.
+
+        Parameters:
+             aa_str: A string of amino acids.
+
+         Returns:
+             A numpy array of the same length as the input string representing
+             the level of selection for each amino acid site.
         """
-        parent_aa_onehot = sequences.aa_one_hot(parent)
+        aa_onehot = sequences.aa_one_hot_tensor(aa_str)
 
         with torch.no_grad():
-            selection_matrix = self.model(parent_aa_onehot)
-        
+            aa_onehot = aa_onehot.to(self.device)
+            model_out = self.model(aa_onehot.unsqueeze(0)).squeeze(0)
 
-class TorchBinaryMutSel(MutSel):
-    """A mutation selection model that is just about mutation vs no mutation, and is trainable."""
+        return model_out.cpu().numpy()[: len(aa_str)]
+
+
+class WrappedBinaryMutSel(MutSel):
+    """A mutation selection model that is built from a model that has a `p_substitution_of_aa_str` method."""
 
     def __init__(self, model, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = model
 
-
-    def build_selection_matrix_from_parent(self, parent):
+    def build_selection_matrix_from_parent(
+        self: TransformerBinarySelectionModel, parent: str
+    ):
         # We need to take our binary selection matrix and turn it into a selection matrix which gives the same weight to each off-diagonal element.
-        selection_matrix = self.model.nograd_forward_of_aa_string(parent)
+        p_substitution = self.model.p_substitution_of_aa_str(parent)
+        parent_idxs = sequences.aa_idx_array_of_str(parent)
 
-    def bselection_matrices_from_parent_aa_onehots(self, parent_onehots: Tensor) -> Tensor:
-        """Build a binary selection matrix from a one-hot encoded parent sequence.
+        # make a np array with the same number of rows as the length of p_substitution
+        # and the same number of columns as the number of amino acids
+        selection_matrix = np.zeros((len(p_substitution), 20))
 
-        Parameters:
-            parent_onehots: A tensor of shape (B, L, 20) representing the one-hot encoding of parent sequences.
+        # Set each row to p_substitution/19, which is the probability of the
+        # corresponding site mutating to a given alternative amino acid.
+        selection_matrix[:, :] = p_substitution[:, np.newaxis] / 19.0
 
-        Returns:
-            A tensor of shape (B, L, 1) representing the level of selection for each amino acid site.
-        """
-        pass
+        # Set "diagonal" elements to 1 - p_substitution for each corresponding amino
+        # acid in the parent, where "diagonal means keeping the same amino acid.
+        selection_matrix[np.arange(len(parent_idxs)), parent_idxs] = (
+            1.0 - p_substitution
+        )
+
+        return selection_matrix
